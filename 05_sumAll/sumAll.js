f65717a9731748eb5cd19ee34384cf3fd94ca3b6
HyunJun Baek <hjbaek5796@gmail.com>
2024-08-01 04:09:49 +0900

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
+  if (numOne < 0 || numTwo < 0) return "ERROR";
+  if (numOne > numTwo) {
+    const temp = numOne;
+    numOne = numTwo;
+    numTwo = temp;
+  }
 
+  let sum = 0;
+  for (let i = numOne; i <= numTwo; i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
