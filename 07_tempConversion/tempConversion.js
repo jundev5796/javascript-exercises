1cba0e84948b6f2c6f321b8bfe7a61f27ebc60b0
HyunJun Baek <hjbaek5796@gmail.com>
2024-08-02 14:35:01 +0900

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  const cel = ((num - 32) * 5) / 9;
+  const round = Math.round(cel * 10) / 10;
+  return round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  const far = (num * 9) / 5 + 32;
+  const round = Math.round(far * 10) / 10;
+  return round;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
