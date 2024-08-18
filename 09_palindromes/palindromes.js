4a6e44ae4e31705a328cf780b7da35537c0270fa
HyunJun Baek <hjbaek5796@gmail.com>
2024-08-18 18:20:26 +0900

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const normalString = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => characters.includes(character))
+    .join("");
+
+  const reversedString = normalString.split("").reverse().join("");
+
+  return normalString === reversedString;
 };
 
 // Do not edit below this line
